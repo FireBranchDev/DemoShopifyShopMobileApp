32a4d163658874f3dc251fbb8d14b8c1e91f3107
Joshua Ryan-Sprowson <dev.joshuars@gmail.com>
2024-02-07 13:52:41 +0800

    Add project files.

---
+++ DemoShopifyShopMobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoShopifyShopMobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DemoShopifyShopMobileApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DemoShopifyShopMobileApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
